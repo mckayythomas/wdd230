074584fa8e406227c93625df32848ccfa0bcdf1d
mckayythomas <mckayycollins@gmail.com>
2022-07-12 13:21:36 -0500

    weather data add

--- weather/js/wind-chill.js
+++ weather/js/wind-chill.js
@@ -3,7 +3,8 @@ function windChill() {
     let temp = parseFloat(document.getElementById("temp").innerHTML);
     let windSpeed = parseFloat(document.getElementById("windSpeed").innerHTML);
     if (temp < 50 && windSpeed > 3) {
-    windChill = 35.74 + 0.6215 * temp - 35.75 * windSpeed**.16 + .4275 * temp * windSpeed ** .16};
-
-    document.getElementById("windChill").innerHTML = windChill.toFixed(0);
+    windChill = 35.74 + 0.6215 * temp - 35.75 * windSpeed**.16 + .4275 * temp * windSpeed ** .16;
+    windChill = windChill.toFixed(0) + ' mph';}
+    else {windChill = 'N/A';}
+    document.getElementById("windChill").innerHTML = windChill;
 }
