f9d18723f9de67a4152d79d1a93a81edb7fa0d8f
mckayythomas <mckayycollins@gmail.com>
2022-07-14 01:09:47 -0500

    final site commit

--- weather/js/fish-haven-weather.js
+++ weather/js/fish-haven-weather.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=c78aa2048fa7d286e4c1c542c60adca2&units=imperial"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=42.0372&lon=111.3960&appid=c78aa2048fa7d286e4c1c542c60adca2&units=imperial"
 
 fetch(apiURL)
     .then((response) => response.json())
@@ -22,7 +22,7 @@ fetch(apiURL)
 
     });
 
-const forcastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&cnt=40&appid=c78aa2048fa7d286e4c1c542c60adca2&units=imperial"
+const forcastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=42.0372&lon=111.3960&cnt=40&appid=c78aa2048fa7d286e4c1c542c60adca2&units=imperial"
 fetch(forcastURL)
     .then((response) => response.json())
     .then((jsObject) => {
@@ -47,44 +47,40 @@ fetch(forcastURL)
 
                 //console.log(dayClass, dayTempClass, dayConditionClass);
                 dayIndex += 1;
-
-
-
-
             }   
         }
     });
 
-const townApi = 'https://byui-cit230.github.io/weather/data/towndata.json'
+    const townApi = 'https://byui-cit230.github.io/weather/data/towndata.json'
 
-fetch(townApi)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const towns = jsonObject['towns'];
-
-    PRESTON_INDEX = 6;
-    // create elements 
-    let card = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let hr = document.createElement('hr');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-
-    // Append elements
-    document.querySelector('div.town-events').appendChild(card)
-    card.appendChild(h2);
-    card.appendChild(hr);
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
-
-    // create text content
-    h2.textContent = 'Upcoming Events:';
-    p1.textContent = towns[PRESTON_INDEX].events[0];
-    p2.textContent = towns[PRESTON_INDEX].events[1];
-    p3.textContent = towns[PRESTON_INDEX].events[2];
-  });
+    fetch(townApi)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+    
+        FISH_HAVEN_INDEX = 2;
+        // create elements 
+        let card = document.createElement('div');
+        let h2 = document.createElement('h2');
+        let hr = document.createElement('hr');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+    
+        // Append elements
+        document.querySelector('div.town-events').appendChild(card)
+        card.appendChild(h2);
+        card.appendChild(hr);
+        card.appendChild(p1);
+        card.appendChild(p2);
+        card.appendChild(p3);
+    
+        // create text content
+        h2.textContent = 'Upcoming Events:';
+        p1.textContent = towns[FISH_HAVEN_INDEX].events[0];
+        p2.textContent = towns[FISH_HAVEN_INDEX].events[1];
+        p3.textContent = towns[FISH_HAVEN_INDEX].events[2];
+      });
